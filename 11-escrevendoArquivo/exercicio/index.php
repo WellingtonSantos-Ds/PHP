17f860c844b5442b18c7847235f52f717dec6dc9
Wellington Santos <xispakix@gmail.com>
2023-11-18 17:29:19 -0300

    up

---
+++ 11-escrevendoArquivo/exercicio/index.php
@@ -0,0 +1,31 @@
+<form action="" method="get">
+<label >
+   NOVO NOME:
+   <br>
+   <input type="text"name="nomes">
+   <button>Adicionar</button>
+</label>
+</form>
+<h1>Listas de nomes</h1>
+
+
+<?php
+
+$nomes = filter_input(INPUT_GET,'nomes',FILTER_SANITIZE_STRING);
+if($nomes){
+  $texto = file_get_contents('nomes.txt');
+  $texto .= "\n$nomes";
+  file_put_contents('nomes.txt',$texto);
+ 
+}else{
+   header('Location:');
+}
+
+$lendo = file_get_contents('nomes.txt');
+$convert = explode("\n",$lendo);
+
+foreach($convert as $todos){
+   echo "<ul>";
+   echo "<li>".$todos."</li>";
+   echo "</ul>";
+}
--- 11-escrevendoArquivo/txtoEmArquivo.php
+++ 11-escrevendoArquivo/txtoEmArquivo.php
@@ -13,17 +13,17 @@ file_put_contents('texto.txt',$texto);
 
 echo 'salvo';
 
-// // Cw pegando texto pelo formulário.
-// $nome = filter_input(INPUT_GET,'name',FILTER_SANITIZE_SPECIAL_CHARS);
-// $texto = filter_input(INPUT_GET,'texto',FILTER_SANITIZE_SPECIAL_CHARS);
-// $txt = '.txt';
-// if($nome && $texto){
-// $tudo = $nome.$txt;
-// file_put_contents("$tudo",$texto);
-// echo 'Salvo ';
-// }else{
-//    echo "erro";
-// }
+// Cw pegando texto pelo formulário.
+$nome = filter_input(INPUT_GET,'name',FILTER_SANITIZE_SPECIAL_CHARS);
+$texto = filter_input(INPUT_GET,'texto',FILTER_SANITIZE_SPECIAL_CHARS);
+$txt = '.txt';
+if($nome && $texto){
+   $tudo = $nome.$txt;
+   file_put_contents("$tudo",$texto);
+   echo 'Salvo ';
+}else{
+   echo "erro";
+}
 
 
 
