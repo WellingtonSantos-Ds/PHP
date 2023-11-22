c395e116057d9108489cf108496f2369b1aae254
Wellington Santos <xispakix@gmail.com>
2023-11-22 09:05:06 -0300

    up

---
+++ 14-manipulaçãoDeArray/04- array_slice.php
@@ -0,0 +1,13 @@
+<?php
+
+$array = ['a','b','c','d','e','f'];
+
+// O array_slice faz uma cópia do array cortando as partis de acordo cam os parâmetros. Primeiro parâmetro o array. Segundo de onde vai começar. O terceiro quantos item vai pegar a partir do segundo parâmetro. Que pode ser negativo aí ele conta de traz para frente.
+$retorna = array_slice($array, 0 , 3);
+// vai retornar de ('a' até 'c');
+print_r($retorna);
+
+
+$retorna = array_slice($array, -3 , 2);
+// vai retornar de ('d' até 'e');
+print_r($retorna);
