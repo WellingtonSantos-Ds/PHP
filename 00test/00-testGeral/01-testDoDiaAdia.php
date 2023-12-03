fbe431326292da95c45d1bab256b2b3a7a2e9e04
WellingtonSantos-Ds <xispakix@gmail.com>
2023-12-03 11:47:17 -0300

    up

--- 00test/00-testGeral/01-testDoDiaAdia.php
+++ 00test/00-testGeral/01-testDoDiaAdia.php
@@ -1,2 +1,20 @@
 <?php
 
+function somar ($a,$b){
+   
+   $valor = $a + $b;
+
+   
+   do{
+      echo $valor."<br>";
+      $valor += 1;
+   }while ($valor < 100);
+      
+   
+    return $valor;
+   
+}
+
+somar(0,95);
+
+
