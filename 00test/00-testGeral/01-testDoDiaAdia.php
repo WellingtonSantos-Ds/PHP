17e7c775ef28f2f052ce011f4fe988b50610bbb5
WellingtonSantos-Ds <xispakix@gmail.com>
2023-12-06 19:54:20 -0300

    up

--- 00test/00-testGeral/01-testDoDiaAdia.php
+++ 00test/00-testGeral/01-testDoDiaAdia.php
@@ -1,4 +1,19 @@
 <?php
 
+function somar($a, $b)
+{
+    return $a + $b;
+}
 
+// Chamando a função somar com dois números
+$resultado1 = somar(2, 2);
+echo $resultado1; // Saída: 4
 
+// Usando uma função anônima como segundo argumento para a função somar
+$resultado2 = somar(10, function ($r, $c) {
+    return $r + $c;
+});
+
+echo $resultado2; // Saída: 10 (resultado da função anônima)
+
+?>
