85f9f4022f4f97d9b963362d258d37eeb93fdf6c
WellingtonSantos-Ds <xispakix@gmail.com>
2023-12-13 16:51:13 -0300

    up

--- 00test/00-testGeral/01-testDoDiaAdia.php
+++ 00test/00-testGeral/01-testDoDiaAdia.php
@@ -1,3 +1,9 @@
  <?php
 
+$text = file_get_contents('lendo.txt');
+echo $text;
+$text .= "\n colocando um nove texto";
+file_put_contents('lendo.txt',$text);
+echo $text;
+
 
--- 0111-esquecido/sobre.php
+++
@@ -1,4 +0,0 @@
-<?php
-require('heder.php');
-require('config.php');
-echo 'exporando nome de config '.$nome;
