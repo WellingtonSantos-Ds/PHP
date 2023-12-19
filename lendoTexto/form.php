5a3ce50c4508bd53efb930716f248f5f412f09ec
WellingtonSantos-Ds <xispakix@gmail.com>
2023-12-19 14:01:46 -0300

    up

---
+++ lendoTexto/form.php
@@ -0,0 +1,14 @@
+
+
+<h1>RECEBENDO O TEXTO</h1>
+
+<form method="get" action="">
+   <label>
+      <input type="text" name="texto">
+   </label>
+   <button type="submit">Enviar</button>
+</form>
+
+<?php
+
+
